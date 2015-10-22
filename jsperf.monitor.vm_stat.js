92833abf923f07191bb6cdd7f03e02dd52235f62
kurohara <kurohara@yk.rim.or.jp>
2015-10-22 17:15:10 +0900

    header comment added

--- jsperf.monitor.vm_stat.js
+++ jsperf.monitor.vm_stat.js
@@ -1,3 +1,11 @@
+/**
+ * jsperf.monitor.vm_stat.js
+ * Monitor module of jsperf for 'vm_stat' command.
+ * This module is for The 'vm_stat' command for 'macosx'.
+ *
+ * Copyright(C) 2015 Hiroyoshi Kurohara(Microgadget,inc.) all rights reserved.
+ */
+
 var fs = require('fs');
 
 var monitor = module.exports = {
