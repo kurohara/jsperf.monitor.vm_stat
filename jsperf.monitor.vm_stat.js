2f343e3d13ab1c18829e580ed981e822d353001a
kurohara <kurohara@yk.rim.or.jp>
2015-10-23 14:53:07 +0900

    monintor interface has changed

--- jsperf.monitor.vm_stat.js
+++ jsperf.monitor.vm_stat.js
@@ -12,6 +12,7 @@ var monitor = module.exports = {
   syntax: fs.readFileSync(require('path').resolve(__dirname,'vm_stat.jison'), 'utf8'),
   oscommand: "/usr/bin/vm_stat",
   args: [ "1" ],
+  dataname: "vm_stat",
 };
 
 
